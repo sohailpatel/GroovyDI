7ac7c7432461dfde84364e02f0d02a3394ece0c7
sohailpatel <sohailpatelt20@gmail.com>
2016-12-13 11:28:17 +0530

    without DI code removed

--- GroovyFirst/src/withoutDI/EmailService.java
+++
@@ -1,8 +0,0 @@
-package withoutDI;
-
-public class EmailService{
-	
-	public void sendemail(String message,String receiver){
-		System.out.println( "Msg send to "+receiver);
-	}
-}
--- GroovyFirst/src/withoutDI/ProvideServices.groovy
+++
@@ -1,9 +0,0 @@
-package withoutDI
-
-class ProvideServices {
-	EmailService emailservice=new EmailService()
-	
-	public void requestMail(String message,String receiver){
-		this.emailservice.sendemail(message, receiver)
-	}
-}
